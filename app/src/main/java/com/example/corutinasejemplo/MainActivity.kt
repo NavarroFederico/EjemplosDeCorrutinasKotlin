96dce244ccdb81ee8345558383e2c0ea5326cd2c
Federico Navarro <navarrofederico14@gmail.com>
2022-10-08 17:48:47 -0300

    Ejemplo de que las corrutinas se ejecutan mas rapido que con el uso de los Thread

--- app/src/main/java/com/example/corutinasejemplo/MainActivity.kt
+++ app/src/main/java/com/example/corutinasejemplo/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.corutinasejemplo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.concurrent.thread
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
+    private val TAG: String = "ActividadPrincipal"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//Corriendo corrutina
+/*        val time = measureTimeMillis {
+            runBlocking {
+                for (i in 1..100_000) {
+                    launch {
+                        delay(10_000L)
+                    }
+                }
+            }
+        }
+       Log.d(TAG,"tiempo transcurrido : $time en ${Thread.currentThread().name}")
+*/
+    val timeHiloMain = measureTimeMillis {
+        for ( i in 1..10_000){
+            Thread {
+                Thread.sleep(1)
+            }.run()
+            }
+        }
+    Log.d(TAG,"tiempo transcurrido : $timeHiloMain en ${Thread.currentThread().name}")
+
     }
+
 }
+
