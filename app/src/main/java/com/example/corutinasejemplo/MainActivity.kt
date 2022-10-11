f698ef8f1af0e4060a3fb5c84ca7e158d32a67a5
Federico Navarro <navarrofederico14@gmail.com>
2022-10-11 12:12:44 -0300

    Ejemplo de corrutina ejecutandose en diferentes contextos. Cambio el contexto de IO a main . Sino lo cambio a main tira error.

--- app/src/main/java/com/example/corutinasejemplo/MainActivity.kt
+++ app/src/main/java/com/example/corutinasejemplo/MainActivity.kt
@@ -2,15 +2,41 @@ package com.example.corutinasejemplo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
-
     private val TAG: String = "ActividadPrincipal"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "Funcion main fue iniciada  ${Thread.currentThread().name}")
+
+
+
+        btn_ejecutar.setOnClickListener {
+
+            GlobalScope.launch(Dispatchers.IO) {
+                val respuesta = obtenerInfoDeInternet()
+                withContext(Dispatchers.Main) {
+                    Log.d(TAG, "Se muestra la respuesta en text vie pantalla ${Thread.currentThread().name}")
+                textView.text = respuesta
+                }
+            }
+        }
+        // Log.d(TAG,respuesta)
+
+    }
+
+
+    suspend fun obtenerInfoDeInternet(): String {
+        Log.d(TAG, "Obteniendo respuesta ${Thread.currentThread().name}")
+        delay(3000L)
+
+        return "Respuesta de Internet"
 
     }
 }
