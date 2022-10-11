05a8dcdcdba9bcd3191854c1f5689f70df587a50
Federico Navarro <navarrofederico14@gmail.com>
2022-10-11 10:04:41 -0300

    Ejemplo : se puede ver como se ejecutan dos corrutinas al mis mo tiempo al presionar el boton ejecutar.

--- app/src/main/java/com/example/corutinasejemplo/MainActivity.kt
+++ app/src/main/java/com/example/corutinasejemplo/MainActivity.kt
@@ -2,12 +2,11 @@ package com.example.corutinasejemplo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings.Global
 import android.util.Log
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlin.concurrent.thread
 import kotlin.system.measureTimeMillis
 
@@ -17,17 +16,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//Corriendo corrutina
-        Log.d(TAG, "Corriendo código 2 en el subproceso : ${Thread.currentThread().name}")
-        Log.d(TAG, "Hola, ")
-        GlobalScope.launch {
-            Log.d(TAG, "Corriendo corrutina en el subproceso: ${Thread.currentThread().name}")
-            delay(5000L)
-            Log.d(TAG, "Mundo")
+        Log.d(TAG, "Funcion main fue iniciada  ${Thread.currentThread().name}")
+
+
+
+        btn_ejecutar.setOnClickListener {
+            GlobalScope.launch(Dispatchers.Main) {
+
+                Log.d(TAG, "Corrutina 1 Iniciada ${Thread.currentThread().name}")
+                obtenerInfoDeInternet()
+                Log.d(TAG, "Corrutina 1 Finalizada")
+            }
+            GlobalScope.launch(Dispatchers.Main) {
+                Log.d(TAG, "Corrutina 2 Iniciada ${Thread.currentThread().name}")
+                textView.setText("esperando")
+                Log.d(TAG, "Corrutina 2 Finalizada")
+            }
         }
-        Log.d(TAG, "Federico en : ${Thread.currentThread().name}")
+
+        // Log.d(TAG,respuesta)
+
     }
 
+
+    suspend fun obtenerInfoDeInternet(): String {
+        Log.d(TAG,"Obteniendo respuesta ${Thread.currentThread().name}")
+        delay(5000L)
+        textView.setText("Respuesta De Internet")
+        return "Respuesta de Internet"
+    }
 }
 
 
